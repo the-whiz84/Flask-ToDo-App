6e9c46541ad09a145237b1d2fa4fb116cc75ef0c
Radu Chiriac <radu@zerodawn.tech>
2024-10-19 00:31:57 +0300

    Adjusted DB URI

--- api/main.py
+++ api/main.py
@@ -48,7 +48,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"{SQLALCHEMY_DATABASE_URI}, sqlite:///todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"{SQLALCHEMY_DATABASE_URI}"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 

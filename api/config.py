4643e2370a26b18199ad6db52aa765c4f21d9085
Radu Chiriac <radu@zerodawn.tech>
2026-02-24 11:30:31 +0200

    Update config.py

--- api/config.py
+++ api/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key_change_in_prod")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///todo.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///todo.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
